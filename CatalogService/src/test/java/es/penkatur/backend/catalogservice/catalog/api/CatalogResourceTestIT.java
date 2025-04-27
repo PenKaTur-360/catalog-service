3025b0dc263c1bfc671c4739bf0d41bd9f17ca86
TaixMiguel <taixmiguel@none.com>
2025-04-27 10:54:08 +0200

    squash: Create domain, application, infrastructure and api layer for Catalog

--- CatalogService/src/test/java/es/penkatur/backend/catalogservice/catalog/api/CatalogResourceTestIT.java
+++
@@ -1,7 +0,0 @@
-package es.penkatur.backend.catalogservice.catalog.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class CatalogResourceTestIT extends CatalogResourceTest {
-}
